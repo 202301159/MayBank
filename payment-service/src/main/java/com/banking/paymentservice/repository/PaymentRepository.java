29e7329c07fc289fc25092bb03a073155e2068df
Dipkumar Zadafiya <202301159@daiict.ac.in>
2026-08-21 22:20:03 +0530

    feat(payment-service): Implemented Webhook handling Success and Failure

--- payment-service/src/main/java/com/banking/paymentservice/repository/PaymentRepository.java
+++ payment-service/src/main/java/com/banking/paymentservice/repository/PaymentRepository.java
@@ -2,6 +2,11 @@ package com.banking.paymentservice.repository;
 
 import com.banking.paymentservice.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<Payment, String> {
+
+    Optional<Payment> findByRazorpayOrderId(String orderId);
 }
